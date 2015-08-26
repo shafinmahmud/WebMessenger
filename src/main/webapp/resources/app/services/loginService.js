2fcc72c36d25377d3bcd5f66ec615fa18da90693
Shafin Mahmud <shafin.mahmud@gmail.com>
2015-08-26 22:01:35 +0600

    Integration#09

--- src/main/webapp/resources/app/services/loginService.js
+++ src/main/webapp/resources/app/services/loginService.js
@@ -1,4 +1,23 @@
 /* 
  */
 
+(function (angular) {
+    angular.module("loginApp.service").service("loginService", function ($http) {
 
+        this.authLogin = function (loginData) {
+            console.log('Service accessed: ');
+            console.log(loginData);
+
+            $http.post('./loginAuth', loginData)
+                    .success(function (data, status, headers, config) {
+                        console.log('success I guss')
+                       
+                    })
+                    .error(function (data, status, headers, config) {
+                        console.log(status + 'failed guss')
+                        
+                    });
+            return '0';
+        };
+    });
+})(angular);
